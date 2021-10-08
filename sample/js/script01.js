de921af924eb6517286edf045df44d06773318d8
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2021-10-08 16:10:15 +0900

    js書き換え

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,15 +1,19 @@
 $(function(){
 
- $("＃button01").on("click",function()
- {$("box back-red").hide();})
+ $("＃button01").on("click",function(){
+     $("box back-red").hide();
+    })
 
- $("button02").on("click",function()
- {$("box back-blue").hide();})
+ 　$("button02").on("click",function(){
+     $("box back-blue").hide();
+    })
 
- $("button03").on("click",function()
- {$("box03,box07").hide();})
+ 　$("button03").on("click",function(){
+     $("box03,box07").hide();
+    })
 
- $("button04").on("click",function()
- {$("box").hide();})
+ 　$("button04").on("click",function(){
+     $("box").hide();
+    })
 
 })
