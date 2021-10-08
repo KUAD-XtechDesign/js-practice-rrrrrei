146db47b05ca112347d7b8a13a0f5bbea3d322d6
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2021-10-08 15:58:38 +0900

    Update script01.js
    js書き換え

---
+++ sample/js/script01.js
@@ -0,0 +1,15 @@
+$(function(){
+
+ $("button01").on("click",function()
+ {$("box back-red").hide();})
+
+ $("button02").on("click",function()
+ {$("box back-blue").hide();})
+
+ $("button03").on("click",function()
+ {$("box03,box07").hide();})
+
+ $("button04").on("click",function()
+ {$("box").hide();})
+
+})
