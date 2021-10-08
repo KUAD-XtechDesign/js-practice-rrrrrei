7c2ffa62d82a9bf989a9704bcfbdf92a21bda0c9
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2021-10-08 16:22:47 +0900

    idコード#追加

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,18 +1,18 @@
 $(function(){
 
- $("＃button01").on("click",function(){
-     $("box back-red").hide();
+ $("#button01").on("click",function(){
+     $(".back-red").hide();
     })
 
- 　$("button02").on("click",function(){
+ 　$("#button02").on("click",function(){
      $("box back-blue").hide();
     })
 
- 　$("button03").on("click",function(){
-     $("box03,box07").hide();
+ 　$("#button03").on("click",function(){
+     $("#box03,#box07").hide();
     })
 
- 　$("button04").on("click",function(){
+ 　$("#button04").on("click",function(){
      $("box").hide();
     })
 
